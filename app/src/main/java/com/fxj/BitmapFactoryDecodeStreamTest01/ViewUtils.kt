fa39c94c287b1caf6cd5c9d10e8945a10879e554
fuxianjin <fuxianjin@qiyi.com>
2021-03-01 14:27:41 +0800

    使用fresco下载图片

--- app/src/main/java/com/fxj/BitmapFactoryDecodeStreamTest01/ViewUtils.kt
+++ app/src/main/java/com/fxj/BitmapFactoryDecodeStreamTest01/ViewUtils.kt
@@ -1,5 +1,6 @@
 package com.fxj.BitmapFactoryDecodeStreamTest01
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
@@ -7,8 +8,18 @@ import android.os.Handler
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import com.facebook.common.executors.UiThreadImmediateExecutorService
+import com.facebook.common.references.CloseableReference
+import com.facebook.datasource.DataSource
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.core.DefaultExecutorSupplier
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber
+import com.facebook.imagepipeline.image.CloseableImage
+import com.facebook.imagepipeline.request.ImageRequest
 import java.io.IOException
 import java.net.URL
+import java.util.concurrent.Executor
+
 
 class ViewUtils {
     companion object{
@@ -31,9 +42,12 @@ class ViewUtils {
                     conn.connect()
                     val mInputStream = conn.getInputStream()
                     val bitmap = BitmapFactory.decodeStream(mInputStream)
-                    Log.d(TAG, "##setBackground##bitmap.byteCount=${if(bitmap!=null) bitmap.byteCount else -1}")
+                    Log.d(
+                        TAG,
+                        "##setBackground##bitmap.byteCount=${if (bitmap != null) bitmap.byteCount else -1}"
+                    )
 
-                    Handler(view.context.mainLooper).post (object : Runnable {
+                    Handler(view.context.mainLooper).post(object : Runnable {
                         override fun run() {
                             if (bitmap != null) {
                                 val bitmapDrawable = BitmapDrawable(view.resources, bitmap)
@@ -48,9 +62,59 @@ class ViewUtils {
                 }
                 Thread(runnable).start()
             } catch (e: IOException) {
-                Log.e(TAG,"##setBackground##IOException,e.message=${e.message},e.localizedMessage=${e.localizedMessage}")
+                Log.e(
+                    TAG,
+                    "##setBackground##IOException,e.message=${e.message},e.localizedMessage=${e.localizedMessage}"
+                )
                 e.printStackTrace()
             }
+        }
+
+        fun setBackgroundByFresco(view: View?, drawableResUrl: String){
+            if(view==null||TextUtils.isEmpty(drawableResUrl)){
+                if(BuildConfig.DEBUG){
+                    throw Exception("请检查传入的view或者drawableResUrl是否为空!")
+                }
+                return;
+            }
+            var imageRequest: ImageRequest=ImageRequest.fromUri(drawableResUrl)
+
+            val imagePipeline = Fresco.getImagePipeline()
+            val dataSource: DataSource<CloseableReference<CloseableImage>> =
+                imagePipeline.fetchDecodedImage(imageRequest, view?.context)
+
+            val cpuAvilableNumbers=Runtime.getRuntime().availableProcessors()
+            Log.d(TAG,"##setBackgroundByFresco##cpuAvilableNumbers=${cpuAvilableNumbers}")
+
+            var executor: Executor= DefaultExecutorSupplier(cpuAvilableNumbers).forBackgroundTasks()
+
+            dataSource.subscribe(
+                object : BaseBitmapDataSubscriber() {
+                    override fun onNewResultImpl(bitmap: Bitmap?) {
+                        Log.d(TAG,"##setBackgroundByFresco.onNewResultImpl##bitmap.byteCount=${if(bitmap!=null) bitmap.byteCount else -1}")
+                        if(bitmap==null){
+                            return
+                        }
+
+                        UiThreadImmediateExecutorService.getInstance().execute(object:Runnable{
+                            override fun run() {
+                                val bitmapDrawable = BitmapDrawable(view.resources, bitmap)
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                                    view.background = bitmapDrawable
+                                } else {
+                                    view.setBackgroundDrawable(bitmapDrawable)
+                                }
+                            }
+                        })
+                    }
+
+                    override fun onFailureImpl(dataSource: DataSource<CloseableReference<CloseableImage>>?) {
+                        Log.d(TAG,"##setBackgroundByFresco.onFailureImpl##")
+                    }
+
+                },
+                executor
+            )
         }
     }
 
